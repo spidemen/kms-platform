06bf47835bf50b7f4126572225a4e1749fa1ac9e
hz-zhengqifeng <zhengqifeng@great-tao.com>
2018-07-10 22:17:19 +0800

    公共前端模块开发

--- src/main/resources/static/js/core.js
+++ src/main/resources/static/js/core.js
@@ -218,6 +218,19 @@ var Core = (function () {
         $(e).removeAttr('disabled');//添加disabled属性
     }
 
+    /*询问框*/
+    core.confirm = function(content,d){
+        layer.confirm(content, {
+            icon: 3,
+            title: "系统提示",
+            btn: ['确认', '取消'],
+            btnclass: ['btn btn-primary', 'btn btn-danger'],
+        }, function (index) {
+            layer.close(index);
+            d(true);
+        });
+    }
+
     //date类型到字符串
     core.formatterDateTime = function (date) {
         var datetime = date.getFullYear()
