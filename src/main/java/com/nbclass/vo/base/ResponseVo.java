2cc14351a10db4e712aa12d157c7a0c8bbbbb862
hz-zhengqifeng <zhengqifeng@great-tao.com>
2018-07-10 22:18:12 +0800

    分页封装已经返回vo优化

---
+++ src/main/java/com/nbclass/vo/base/ResponseVo.java
@@ -0,0 +1,38 @@
+package com.nbclass.vo.base;
+
+public class ResponseVo<T> {
+    private String status;
+    private String msg;
+    private T data;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public ResponseVo(String status, String msg, T data) {
+        this.status = status;
+        this.msg = msg;
+        this.data = data;
+    }
+
+}
