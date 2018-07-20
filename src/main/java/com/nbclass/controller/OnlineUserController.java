77cf7722b03d95379eba53ef8390376f3f375613
jax <523179414@qq.com>
2018-07-21 00:01:26 +0800

    禁止踢出自己

--- src/main/java/com/nbclass/controller/OnlineUserController.java
+++ src/main/java/com/nbclass/controller/OnlineUserController.java
@@ -6,10 +6,12 @@ import com.nbclass.vo.UserOnlineVo;
 import com.nbclass.vo.UserSessionVo;
 import com.nbclass.vo.base.PageResultVo;
 import com.nbclass.vo.base.ResponseVo;
+import org.apache.shiro.SecurityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.Serializable;
 import java.util.List;
 
 @Controller
@@ -32,6 +34,9 @@ public class OnlineUserController {
     @ResponseBody
     public ResponseVo kickout(String sessionId,String username) {
         try {
+            if(SecurityUtils.getSubject().getSession().getId().equals(sessionId)){
+                return ResultUtil.error("不能踢出自己");
+            }
             userService.kickout(sessionId,username);
             return ResultUtil.success("踢出用户成功");
         } catch (Exception e) {
@@ -44,8 +49,20 @@ public class OnlineUserController {
     @ResponseBody
     public ResponseVo kickout(@RequestBody List<UserSessionVo> sessions) {
         try {
+            //要踢出的用户中是否有自己
+            boolean hasOwn=false;
+            Serializable sessionId = SecurityUtils.getSubject().getSession().getId();
             for (UserSessionVo sessionVo : sessions) {
-                userService.kickout(sessionVo.getSessionId(),sessionVo.getUsername());
+                if(sessionVo.getSessionId().equals(sessionId)){
+                    hasOwn=true;
+                }else{
+                    userService.kickout(sessionVo.getSessionId(),sessionVo.getUsername());
+                }
+
+
+            }
+            if(hasOwn){
+                return ResultUtil.success("不能踢出自己");
             }
             return ResultUtil.success("踢出用户成功");
         } catch (Exception e) {
